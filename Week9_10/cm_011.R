77706b98647e60b6abf5686ac04f06c39e703045
almas2019 <almas.khan@alumni.ubc.ca>
2019-10-08 10:45:30 -0700

    included the Firas_mri dataset

---
+++ Week9_10/cm_011.R
@@ -0,0 +1,30 @@
+## Read Excel file 
+data_url <- "http://gattonweb.uky.edu/sheather/book/docs/datasets/GreatestGivers.xls"
+# read_csv can directly import files, but read_excel requires you to first download it
+library(here)
+library(tidyverse)
+
+download.file(url=data_url,
+              destfile = here::here("Week9_10","greatestGivers.xls"))
+
+file_name <- basename(data_url)                            
+#Recommendation use basename 
+
+library(readxl)
+philanthropists <- read_excel(here::here("Week9_10",file_name),trim_ws=TRUE)
+View(philanthropists)
+
+head(philanthropists)
+
+download.file(url="https://github.com/STAT545-UBC-hw-2019-20/Announcements/files/3703782/Firas-MRI.xlsx",
+              destfile = here::here("Week9_10","Firas_MRI.xlsx"))
+mri_file=here("Week9_10","Firas_MRI.xlsx")
+mri <- read_xlsx(mri_file,range="A1:L12")
+View(mri)
+mri <- mri[,-10]
+
+mri <- mri %>%
+  pivot_longer(cols='Slice 1':'Slice 8',
+               names_to='slice_no',
+               values_to='value'))
+View(mri)
