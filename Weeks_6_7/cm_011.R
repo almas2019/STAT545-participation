bef581e6ecf63bbfd9ea2de3d8c31ef61da40f13
almas2019 <almas.khan@alumni.ubc.ca>
2019-10-20 21:41:18 -0700

    combined gapminder.R and cm011.R (found 2 .R files from this class)

--- Weeks_6_7/cm_011.R
+++ Weeks_6_7/cm_011.R
@@ -1,3 +1,29 @@
+ibrary("tidyverse")
+library("gapminder")
+
+gapminder 
+write_csv(gapminder,'/.gapminder.csv')
+view(gapminder)
+
+#Calculate mean life Exp per continent
+gapminder_sumn <- gapminder %>% 
+  group_by(continent) %>%
+  summarise(ave_lifeExp=mean(lifeExp))
+
+View(gapminder_sumn)
+write_csv(gapminder_sumn, './gapminder_sum.csv')
+#on windows: .\gapminder_sum.csv
+
+gapminder_sumn %>% 
+  ggplot(aes(x=continent , y=ave_lifeExp))+
+  geom_point()+
+  theme_bw()
+library("here")
+ls()
+# another way using here 
+#platform agnostic , dir-structure agnostic, allows you to set wd 
+# write_csv(here::("test", "tes","te","t","gapminder_sum.csv"))
+
 ## Read Excel file 
 data_url <- "http://gattonweb.uky.edu/sheather/book/docs/datasets/GreatestGivers.xls"
 # read_csv can directly import files, but read_excel requires you to first download it
